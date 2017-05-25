e10ac06470d605924e423f8982b2059fe796a67e
hjc-V11P <13093789869@163.com>
2017-05-25 12:41:56 +0800

    v2

--- blog-webpage/js/main.js
+++ blog-webpage/js/main.js
@@ -1,1 +1,57 @@
-console.log('hello');
+
+var sidebar  = $('#sidebar');
+var mask = document.querySelector('.mask');
+var sidebar_trigger = document.querySelector('#sidebar_trigger');
+//var bttButton = document.querySelector('#backToTop');
+var bttButton= $('#backToTop');
+
+
+
+
+function showSidebar(){
+	//sidebar.style.display= 'block';
+	event.preventDefault();
+	mask.style.display = 'block';
+	//sidebar.css('top',document.body.scrollTop);
+	sidebar.css('right',0);
+
+
+}
+
+function hideSidebar(){
+	mask.style.display='none';
+	sidebar.css('right',-300);
+}
+
+function backToTop(){
+	
+	
+	$('html,body').animate({
+		scrollTop:0},800)
+
+
+}
+
+function hideBttbutton(){
+	//console.log(document.body.scrollTop);
+	//console.log(window.innerHeight);
+	var top = document.documentElement.scrollTop || document.body.scrollTop;
+	if(top < window.innerHeight)
+	{
+		bttButton.fadeOut();
+	}
+	else
+	{
+		bttButton.fadeIn();
+	}
+}
+
+sidebar_trigger.addEventListener('click',showSidebar,true);
+
+mask.addEventListener('click',hideSidebar,true);
+
+//bttButton.addEventListener('click',backToTop,true);
+
+bttButton.on('click',backToTop);
+
+window.addEventListener('scroll',hideBttbutton,true);
